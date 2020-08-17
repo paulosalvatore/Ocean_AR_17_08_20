3ee57edc3924c93fd824f0f356ce64636a6920e2
Paulo Salvatore <paulo@salvatore.tech>
2020-08-17 16:46:10 -0300

    AR Placement criado e target adicionado
    Pastas organizadas

---
+++ Assets/Scripts/ARPlacement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARPlacement : MonoBehaviour
+{
+    public ARRaycastManager rayManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
