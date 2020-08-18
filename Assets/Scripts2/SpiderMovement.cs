46d5a8f9b8a771ce0dbf4ce9d7365e7f6a2eae6c
Paulo Salvatore <paulo@salvatore.tech>
2020-08-18 16:56:24 -0300

    Spider rotation

--- Assets/Scripts2/SpiderMovement.cs
+++ Assets/Scripts2/SpiderMovement.cs
@@ -6,10 +6,35 @@ public class SpiderMovement : MonoBehaviour
 {
     public float speed = 0.2f;
 
+    public float rotationSpeed = 5f;
+
     public Vector3 targetPosition;
+
+    public Animator anim;
 
     private void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+        var initialPosition = transform.position;
+
+        var finalPosition = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+
+        transform.position = finalPosition;
+
+        var direction = finalPosition - initialPosition;
+
+        var distance = Vector3.Distance(finalPosition, initialPosition);
+
+        if (distance > 0 && direction != Vector3.zero)
+        {
+            var targetRotation = Quaternion.LookRotation(direction);
+
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+            anim.SetBool("IsWalking", true);
+        }
+        else
+        {
+            anim.SetBool("IsWalking", false);
+        }
     }
 }
